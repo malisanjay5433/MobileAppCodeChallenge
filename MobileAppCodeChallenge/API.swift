aa87f4013413115096db91520b9a25954ad02fb4
Sanjay Mali <malisanjay5433@gmail.com>
2023-12-30 18:19:18 +0530

    feat: add NetworkManager for network request
    refactor : add  for endpoints and  for clientId

---
+++ MobileAppCodeChallenge/API.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MobileAppCodeChallenge
+//
+//  Created by Sanjay Mali on 30/12/23.
+//
+
+import Foundation
+
+struct API{
+    static let baseURL = "https://api.imgur.com/3/gallery/search/"
+}
---
+++ MobileAppCodeChallenge/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  MobileAppCodeChallenge
+//
+//  Created by Sanjay Mali on 30/12/23.
+//
+
+import Foundation
+
+//TODO: We can store this in Keychain or Some configuration file.
+struct AppConstants {
+    static let clientID = "47eede719e679cb"
+}
