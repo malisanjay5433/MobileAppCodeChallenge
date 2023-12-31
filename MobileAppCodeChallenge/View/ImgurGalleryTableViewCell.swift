76c20f3811037f41842ed1b78f6f2627ed7c13ad
Sanjay Mali <malisanjay5433@gmail.com>
2023-12-31 17:00:37 +0530

    feat: add custom ImgurGalleryTableViewCell for tableview display

---
+++ MobileAppCodeChallenge/View/ImgurGalleryTableViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  ImgurGalleryTableViewCell.swift
+//  MobileAppCodeChallenge
+//
+//  Created by Sanjay Mali on 30/12/23.
+//
+
+import UIKit
+import SDWebImage
+protocol ImgurImageFormatter {
+    func format(date: Int) -> String
+}
+
+class DefaultImgurImageFormatter: ImgurImageFormatter {
+    func format(date: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(date))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yy hh:mm a"
+        return dateFormatter.string(from: date)
+    }
+}
+
+class ImgurGalleryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imgurImageView: UIImageView!
+    @IBOutlet weak var imageCountLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    private var imgurImageFormatter: ImgurImageFormatter!
+    private let formatter: ImgurImageFormatter = DefaultImgurImageFormatter()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    var imgurGalleryData: ImgurGallery? {
+        didSet {
+            guard let imgurGalleryData = imgurGalleryData else {return}
+            self.titleLbl.text = imgurGalleryData.title
+            
+            guard let datetime = imgurGalleryData.datetime  else { return }
+            self.dateLbl.text = formatter.format(date: datetime)
+           
+            guard let imagesCount = imgurGalleryData.images_count else {return}
+            self.imageCountLbl.text = "\(imagesCount)"
+        }
+    }
+    
+    var imageData: Image? {
+        didSet {
+            guard let imageData = imageData else { return }
+            if isImageType(imageData.type) {
+                loadImageFromURL(imageData.link)
+            }
+        }
+    }
+    
+    private func isImageType(_ type: String?) -> Bool {
+        return type == "image/jpeg" || type == "image/jpg"
+    }
+    
+    private func loadImageFromURL(_ urlString: String) {
+        print("URL STRING : \(urlString)")
+        imgurImageView.sd_setImage(with: URL(string: urlString), placeholderImage: UIImage(named: "placeholder"))
+    }
+}
+
