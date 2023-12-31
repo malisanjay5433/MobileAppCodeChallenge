2d228ee566cdc1cc58633ccd7cc81edbbd53d63f
Sanjay Mali <malisanjay5433@gmail.com>
2023-12-31 17:17:32 +0530

    refactor: project restructure and add all files

--- Pods/Target Support Files/Pods-MobileAppCodeChallenge/Pods-MobileAppCodeChallenge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppCodeChallenge : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppCodeChallenge
-@end
--- Pods/Target Support Files/Pods-MobileAppCodeChallengeTests/Pods-MobileAppCodeChallengeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppCodeChallengeTests : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppCodeChallengeTests
-@end
